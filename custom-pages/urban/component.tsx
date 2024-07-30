25742cbea8aa3a5fac2a626c938856a33e17d40d
Hanbyul Jo <hanbyul@nyu.edu>
2024-07-30 11:01:17 -0400

    WIP

--- custom-pages/urban/component.tsx
+++ custom-pages/urban/component.tsx
@@ -1,31 +1,77 @@
-import React from '$veda-ui/react';
+import React, { useState } from '$veda-ui/react';
 import styled from '$veda-ui/styled-components';
+import format from '$veda-ui/date-fns/esm/format';
+import { veda_faux_module_datasets } from '$veda-ui-scripts/data-layer/datasets';
+import MapBlock from '$veda-ui-scripts/components/common/blocks/block-map';
+import Map, { MapControls } from '$veda-ui-scripts/components/common/map';
 import { Basemap } from '$veda-ui-scripts/components/common/map/style-generators/basemap';
-import Map from '$veda-ui-scripts/components/common/map';
-import { DEFAULT_MAP_STYLE_URL } from '$veda-ui-scripts/components/common/mapbox/map-options/basemaps';
+import { LayerLegend, LayerLegendContainer } from '$veda-ui-scripts/components/common/map/layer-legend';
+import { Layer } from '$veda-ui-scripts/components/exploration/components/map/layer';
+import { TimelineDatePicker as DatePicker } from '$veda-ui-scripts/components/exploration/components/timeline/timeline-datepicker';
+import { getDatasetLayers, reconcileDatasets } from '$veda-ui-scripts/components/exploration/data-utils-no-faux-module';
+
+import InteractiveMarkerLayer from './custom-layer';
+
+
 const Carto = styled.div`
   position: relative;
-  flex-grow: 1;
+  grid-column: 1 / -1;
   height: 800px;
 `;
 
-const mapOptions = {
-  style: DEFAULT_MAP_STYLE_URL,
-  logoPosition: 'bottom-left',
-  trackResize: true,
-  pitchWithRotate: false,
-  dragRotate: false,
-  zoom: 1,
-  center: [0,0]
-}
+const StyledMapBlock = styled(MapBlock)`
+  height: 800px !important;
+`
+
+const DatePickerWrapper = styled.div`
+  position: absolute;
+  top: 100px;
+  right: 100px;
+  z-index: 10000;
+  background: white;
+  padding: 10px;
+`
 
 export default function UrbanMap(){
-  return (<Carto id="carto-box">
-    <Map
-      id={'test-custom'}
-      mapOptions={mapOptions}
-    >
-    <Basemap basemapStyleId='satellite' />
-  </Map>
-</Carto>)
+  const [selectedDate, setSelectedDate] = useState(new Date('2022-02-03'));
+  const layers = getDatasetLayers(veda_faux_module_datasets);
+  const layerToDisplay = reconcileDatasets(['oco2-dryair-column'], layers, [])[0];
+  
+  return (
+    <Carto id="carto-box">
+      <Map id="test-map">
+        <Basemap basemapStyleId={'satellite'} />
+        <Layer
+            key={'oco2-dryair-column'}
+            id={layerToDisplay.data.id}
+            dataset={layerToDisplay}
+            selectedDay={new Date(selectedDate)}
+          />
+          <InteractiveMarkerLayer
+            id="interactive-marker-layer"
+            filePath = '/test-data.geojson'
+            onClick={(feature)=> {console.log(feature)}}
+          />
+          <LayerLegendContainer>
+            <LayerLegend
+              id={layerToDisplay.data.id}
+              title={layerToDisplay.data.name}
+              description={layerToDisplay.data.description}
+              {...layerToDisplay.data.legend}
+            />
+            </LayerLegendContainer>
+      </Map>
+    <DatePickerWrapper>
+      <DatePicker 
+        triggerHeadReference='Selected Date '
+        selectedDay={selectedDate}
+        onConfirm={(d) => {
+          if (!d) return;
+          setSelectedDate(new Date(d));
+        }}
+        disabled={false}
+        tipContent={'some content'}
+        />
+    </DatePickerWrapper>
+  </Carto>)
 }
---
+++ custom-pages/urban/custom-layer.tsx
@@ -0,0 +1,213 @@
+import { useCallback, useEffect, useMemo, useState } from '$veda-ui/react';
+import {
+  AnyLayer,
+  AnySourceImpl,
+  VectorSourceImpl
+} from '$veda-ui/mapbox-gl';
+import { useTheme } from '$veda-ui/styled-components';
+import { endOfDay, startOfDay } from 'date-fns';
+import centroid from '$veda-ui/@turf/centroid';
+import { LngLatLike } from '$veda-ui/react-map-gl';
+import { Feature } from '$veda-ui/geojson';
+
+import { BaseGeneratorParams } from '$veda-ui-scripts/components/common/map/types';
+import useMapStyle from '$veda-ui-scripts/components/common/map/hooks/use-map-style';
+import {
+  requestQuickCache
+} from '$veda-ui-scripts/components/common/map/utils';
+import useFitBbox from '$veda-ui-scripts/components/common/map/hooks/use-fit-bbox';
+import useLayerInteraction from '$veda-ui-scripts/components/common/map/hooks/use-layer-interaction';
+import { MARKER_LAYOUT } from '$veda-ui-scripts/components/common/map/hooks/use-custom-marker';
+import useMaps from '$veda-ui-scripts/components/common/map/hooks/use-maps';
+import useGeneratorParams from '$veda-ui-scripts/components/common/map/hooks/use-generator-params';
+
+
+import {
+  ActionStatus,
+  S_FAILED,
+  S_LOADING,
+  S_SUCCEEDED
+} from '$veda-ui-scripts/utils/status';
+import { userTzDate2utcString } from '$veda-ui-scripts/utils/date';
+
+export interface VectorTimeseriesProps extends BaseGeneratorParams {
+  id: string;
+  stacCol: string;
+  date: Date;
+  sourceParams?: Record<string, any>;
+  zoomExtent?: number[];
+  bounds?: number[];
+  onStatusChange?: (result: { status: ActionStatus; id: string }) => void;
+  isPositionSet?: boolean;
+  stacApiEndpoint?: string;
+  filePath: string;
+  onClick: (feature: Feature)=> void;
+}
+
+
+export default function InteractiveMarkerLayer(props: VectorTimeseriesProps) {
+  const {
+    id,
+    date,
+
+    zoomExtent,
+    bounds,
+    onStatusChange,
+    isPositionSet,
+    hidden,
+    opacity,
+    filePath, 
+    onClick
+  } = props;
+
+  const { current: mapInstance } = useMaps();
+  const [sourceData, setSourceData] = useState(null)
+
+  const theme = useTheme();
+  const { updateStyle } = useMapStyle();
+
+  const generatorId = `custom-interactive-marker`;
+
+  const [minZoom, maxZoom] = [18,20];
+  const vectorOpacity = 1;
+  //
+  // Get the tiles url
+
+  const generatorParams = useGeneratorParams({generatorOrder: 10000, hidden: false, opacity: vectorOpacity});
+  //
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function load() {
+      try {
+        
+        const data = await requestQuickCache<any>({
+          url: filePath,
+          method: 'GET',
+          controller
+        });
+        console.log('success')
+        console.log(data);
+        setSourceData(data);
+
+        onStatusChange?.({ status: S_SUCCEEDED, id });
+      } catch (error) {
+        if (!controller.signal.aborted) {
+          setSourceData(null);
+          onStatusChange?.({ status: S_FAILED, id });
+        }
+        return;
+      }
+    }
+
+    load();
+
+    return () => {
+      controller.abort();
+    };
+  }, [id, onStatusChange]);
+
+
+  useEffect(() => {
+    if (!sourceData ) return;
+
+    const sources: Record<string, AnySourceImpl> = {
+      [id]: {
+        type: 'geojson',
+        data: sourceData
+      } as VectorSourceImpl
+    };
+    console.log(sourceData)
+
+    const layers = [
+        {
+            type: 'symbol',
+            id: `${id}-points`,
+            source: id,
+            layout: {
+              ...(MARKER_LAYOUT as any),
+              visibility: hidden ? 'none' : 'visible'
+            },
+            paint: {
+              'icon-color': theme.color?.infographicB,
+              'icon-halo-color': theme.color?.base,
+              'icon-halo-width': 1
+            },
+            maxzoom: minZoom,
+            metadata: {
+              layerOrderPosition: 'markers'
+            }
+          }
+        
+    ].filter(Boolean) as AnyLayer[];
+
+    updateStyle({
+      generatorId,
+      sources,
+      layers,
+      params: generatorParams
+    });
+    // sourceParams not included, but using a stringified version of it to
+    // detect changes (haveSourceParamsChanged)
+    // `theme` will not change throughout the app use
+  }, [
+    id,
+    updateStyle,
+    date,
+    sourceData,
+    minZoom,
+    maxZoom,
+    hidden,
+    opacity,
+    
+    
+    generatorId
+  ]);
+
+  //
+  // Cleanup layers on unmount.
+  //
+  useEffect(() => {
+    return () => {
+      updateStyle({
+        generatorId,
+        sources: {},
+        layers: []
+      });
+    };
+  }, [updateStyle, generatorId]);
+
+  //
+  // Listen to mouse events on the markers layer
+  //
+  const onPointsClick = useCallback(
+    (features) => {
+      const extractedFeat = {
+        type: 'Feature',
+        ...features[0]
+      } as Feature<any>;
+
+      // const center = centroid(extractedFeat).geometry.coordinates as LngLatLike;
+
+      // // Zoom past the min zoom centering on the clicked feature.
+      // mapInstance?.flyTo({
+      //   zoom: minZoom,
+      //   center
+      // });
+      onClick(extractedFeat);
+    },
+    [mapInstance, minZoom]
+  );
+
+  useLayerInteraction({
+    layerId: `${id}-points`,
+    onClick: onPointsClick
+  });
+
+  //
+  // FitBounds when needed
+  //
+  // useFitBbox(!!isPositionSet, bounds, featuresBbox);
+
+  return null;
+}
