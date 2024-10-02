2fd8702bb472a1e0eba45d893a8d8f8a5133556a
Alex Gurvich <alexander.b.gurvich@nasa.gov>
2024-10-02 10:08:50 -0400

    update carousel titles and captions

--- stories/locfeature.UNVEILTIME/carousel_content.jsx
+++ stories/locfeature.UNVEILTIME/carousel_content.jsx
@@ -1,73 +1,73 @@
 const carouselContent = [
     {
         "src":new URL("./Output-Data-Set-1-2.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Athabasca Oil Sands",
+        "caption":"July 1984 - July 2016"
     },
     {
         "src":new URL("./Output-Data-Set-3-4.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Deforestation in Bolivia",
+        "caption":"July 2, 1986 - August 22, 2022"
     },
     {
         "src":new URL("./Output-Data-Set-5-6.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Cancun's Development",
+        "caption":"March 28, 1985 - April 11, 2019"
     },
     {
         "src":new URL("./Output-Data-Set-7-8.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Drying Up of Cerro Prieto Reservoir, Mexico",
+        "caption":"July 20, 2015 - July 7, 2022"
     },
     {
         "src":new URL("./Output-Data-Set-9-10.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Ice Shelf Collapse in East Antarctica",
+        "caption":"January 9, 2022 - March 23, 2022"
     },
     {
         "src":new URL("./Output-Data-Set-11-12.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Urbanization of Dubai",
+        "caption":"2000 - 2011"
     },
     {
         "src":new URL("./Output-Data-Set-13-14.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Sea Ice Declines in Hudson Bay",
+        "caption":"July 13, 2020 - July 29, 2020"
     },
     {
         "src":new URL("./Output-Data-Set-15-16.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Greening of the Inner Niger Delta, Mali",
+        "caption":"May 12, 2022 - Nov. 3, 2022"
     },
     {
         "src":new URL("./Output-Data-Set-17-18.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Floods Swamp Australia's Northern Territory",
+        "caption":"March 5, 2022 - March 10, 2023"
     },
     {
         "src":new URL("./Output-Data-Set-19-20.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Water Level Drops in Argentina's Paraná River",
+        "caption":"July 1, 2019 - July 3, 202"
     },
     {
         "src":new URL("./Output-Data-Set-21-22.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Population Sprawling Shanghai",
+        "caption":"1984 - 2017"
     },
     {
         "src":new URL("./Output-Data-Set-23-24.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"California Reservoir Rebound",
+        "caption":"November 18, 2022 - May 29, 2023"
     },
     {
         "src":new URL("./Output-Data-Set-25-26.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Snowpack in the Sierra Nevada",
+        "caption":"2006 - 2024"
     },
     {
         "src":new URL("./Output-Data-Set-27-28.png", import.meta.url).href,
-        "title":"",
-        "caption":""
+        "title":"Burn Recovery in Yellowstone",
+        "caption":"1987-1989"
     }
 ];
 
