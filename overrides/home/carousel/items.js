a31b53bcdfa5a8b75e5132906360a447a83f6fbb
freitagb <brian.freitag@nsstc.uah.edu>
2024-11-12 13:11:29 -0600

    updated text in carousel

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -1,22 +1,20 @@
 export default [
     {
       link: 'https://earth.gov/ghgcenter',
-      title: 'U.S. Greenhouse Gas Center',
-      description: "Uniting Data and Technology to Empower Tomorrow's Climate Solutions",
+      description: "The U.S. Greenhouse Gas Center (US GHG Center) is a multi-agency effort consolidating greenhouse gas information from observations and models.",
       image: new URL('../media/earth-1.png', import.meta.url).href,
       imageAlt: 'Earth with Greenhouse Gas Emissions shaded in red'
     },
     {
       link: 'https://earth.gov/sealevel',
-      title: 'Global Sea Level Rise',
-      description: 'Rising Seas, Changing Coasts',
+      description: 'The Global Sea Level Rise Portal offers essential information on current and project sea level rise to inform coastal communities across the globe.',
       image: new URL('../media/global-slr.jpg', import.meta.url).href,
       imageAlt: 'Coastline with crashing waves'
     },
     {
     link: 'https://earth.gov/sealevel/us',
     title: 'U.S. Sea Level Rise',
-    description: 'Rising Seas, Changing Coasts',
+    description: ' The U.S. Sea Level Rise portal is a federally supported data visualizations coupled with explanations and science education to help communities prepare for challenges that will affect our coastal environments.',
     image: new URL('../media/seaside-background.jpg', import.meta.url).href,
     imageAlt: 'Coastline with crashing waves'
   }]
