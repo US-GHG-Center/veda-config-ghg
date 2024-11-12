145df0779e5db7d9ba8f5e86d5576decc5287abf
Brian Freitag <freitagb@users.noreply.github.com>
2024-11-12 12:14:16 -0600

    fix path to images

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -3,20 +3,20 @@ export default [
       link: 'https://earth.gov/ghgcenter',
       title: 'U.S. Greenhouse Gas Center',
       description: "Uniting Data and Technology to Empower Tomorrow's Climate Solutions",
-      image: new URL('../media/earth-1.jpg', import.meta.url).href,
+      image: new URL('../../media/earth-1.jpg', import.meta.url).href,
       imageAlt: 'Earth with Greenhouse Gas Emissions shaded in red'
     },
     {
       link: 'https://earth.gov/sealevel',
       title: 'Global Sea Level Rise',
       description: 'Rising Seas, Changing Coasts',
-      image: new URL('../media/global-slr.jpg', import.meta.url).href,
+      image: new URL('../../media/global-slr.jpg', import.meta.url).href,
       imageAlt: 'Coastline with crashing waves'
     },
     {
     link: 'https://earth.gov/sealevel/us',
     title: 'U.S. Sea Level Rise',
     description: 'Rising Seas, Changing Coasts',
-    image: new URL('../media/seaside-background.jpg', import.meta.url).href,
+    image: new URL('../../media/seaside-background.jpg', import.meta.url).href,
     imageAlt: 'Coastline with crashing waves'
   }]
