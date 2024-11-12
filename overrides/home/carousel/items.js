9d84bcee9a627eec810fe5d35d1b60d16bfcf6ed
Brian Freitag <freitagb@users.noreply.github.com>
2024-11-12 11:36:41 -0600

    Add content for earth.gov carousel

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -1,22 +1,22 @@
 export default [
     {
-      link: 'https://eyes.nasa.gov/apps/earth/#/',
-      title: 'Eyes on Earth',
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae lorem et neque egestas mollis. Praesent ut porttitor mauris, a sagittis erat",
-      image: new URL('../media/seaside-background.jpg', import.meta.url).href,
-      imageAlt: 'Example image alt EIC 2024'
+      link: 'https://earth.gov/ghgcenter,
+      title: 'U.S. Greenhouse Gas Center',
+      description: "Uniting Data and Technology to Empower Tomorrow's Climate Solutions",
+      image: new URL('../media/earth-1.jpg', import.meta.url).href,
+      imageAlt: 'Earth with Greenhouse Gas Emissions shaded in red'
     },
     {
-      link: 'https://earth.gov/mobile-climate-mapper/',
-      title: 'Card Title 2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae lorem et neque egestas mollis. Praesent ut porttitor mauris, a sagittis erat',
-      image: new URL('../media/swamp.png', import.meta.url).href,
-      imageAlt: 'Example image alt EIC 2024'
+      link: 'https://earth.gov/sealevel',
+      title: 'Global Sea Level Rise',
+      description: 'Rising Seas, Changing Coasts',
+      image: new URL('../media/global-slr.jpg', import.meta.url).href,
+      imageAlt: 'Coastline with crashing waves'
     },
     {
-    link: '/stories/agriculture',
-    title: 'Internal link to the Agriculture story',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae lorem et neque egestas mollis. Praesent ut porttitor mauris, a sagittis erat',
-    image: new URL('../media/hyperwall-image1.png', import.meta.url).href,
-    imageAlt: 'Example image alt EIC 2024'
+    link: 'https://earth.gov/sealevel/us',
+    title: 'U.S. Sea Level Rise',
+    description: 'Rising Seas, Changing Coasts',
+    image: new URL('../media/seaside-background.jpg', import.meta.url).href,
+    imageAlt: 'Coastline with crashing waves'
   }]
