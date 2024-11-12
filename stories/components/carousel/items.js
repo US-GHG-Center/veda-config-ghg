1e22710c6eefd8b43bdb25b74f9842f7dfe4effd
freitagb <brian.freitag@nsstc.uah.edu>
2024-11-12 13:49:17 -0600

    added carousel on SLR themes page

---
+++ stories/components/carousel/items.js
@@ -0,0 +1,15 @@
+export default [
+    {
+      link: 'https://earth.gov/sealevel',
+      title: 'Global Sea Level Rise',
+      description: 'The Global Sea Level Rise Portal offers essential information on current and project sea level rise to inform coastal communities across the globe.',
+      image: new URL('../media/global_slr.png', import.meta.url).href,
+      imageAlt: 'Coastline with crashing waves'
+    },
+    {
+    link: 'https://earth.gov/sealevel/us',
+    title: 'U.S. Sea Level Rise',
+    description: ' The U.S. Sea Level Rise portal is a federally supported data visualizations coupled with explanations and science education to help communities prepare for challenges that will affect our coastal environments.',
+    image: new URL('../media/us_slr.png', import.meta.url).href,
+    imageAlt: 'Coastline with crashing waves'
+  }]
