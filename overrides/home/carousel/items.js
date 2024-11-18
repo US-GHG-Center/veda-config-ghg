2d9e9ca285a4a6760b2bf7b6cb36604d24eecbb7
Brian Freitag <freitagb@users.noreply.github.com>
2024-11-18 09:29:04 -0600

    update carousel with HQ proposed text

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -8,15 +8,15 @@ export default [
     },
     {
       link: 'https://earth.gov/sealevel',
-      title: 'Global Sea Level Rise',
-      description: 'The Global Sea Level Rise Portal is an international collaboration that offers essential information on current and project sea level rise to inform coastal communities across the globe.',
+      title: 'Global Sea Level Change Portal',
+      description: 'The Global Sea Level Change Portal is an international collaboration that offers essential information on current and projected sea level rise to inform coastal communities across the globe.',
       image: new URL('../media/global_slr.png', import.meta.url).href,
       imageAlt: 'Satellite image of coastline'
     },
     {
     link: 'https://earth.gov/sealevel/us/',
-    title: 'U.S. Sea Level Rise',
-    description: 'The U.S. Sea Level Rise portal is a multi-agency effort pairing data visualizations with explanation and science eduction to help communities prepare for challenges that will affect our coastal environments',
+    title: 'U.S. Sea Level Change Portal',
+    description: 'The U.S. Sea Level Change Portal is a multi-agency effort pairing data visualizations with accessible explanations and science eduction to help communities prepare for challenges that are impacting communities across the coastlines of the United States.',
     image: new URL('../media/us_slr.png', import.meta.url).href,
     imageAlt: 'Coastline with crashing waves'
   }]
