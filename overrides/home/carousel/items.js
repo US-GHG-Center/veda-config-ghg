45e3a513dd5be3881b49f58b6ea47197d82024dd
freitagb <brian.freitag@nsstc.uah.edu>
2024-11-19 09:32:52 -0600

    update language on carousel images

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -10,13 +10,13 @@ export default [
       link: 'https://earth.gov/sealevel',
       title: 'Global Sea Level Change Portal',
       description: 'The Global Sea Level Change Portal is an international collaboration that offers essential information on current and projected sea level rise to inform coastal communities across the globe.',
-      image: new URL('../media/global_slr.png', import.meta.url).href,
+      image: new URL('../media/global_slr_v2.jpg', import.meta.url).href,
       imageAlt: 'Satellite image of coastline'
     },
     {
     link: 'https://earth.gov/sealevel/us/',
     title: 'U.S. Sea Level Change Portal',
     description: 'The U.S. Sea Level Change Portal is a multi-agency effort pairing data visualizations with accessible explanations and science eduction to help communities prepare for challenges that are impacting communities across the coastlines of the United States.',
-    image: new URL('../media/us_slr.png', import.meta.url).href,
+    image: new URL('../media/us_slr_v2.jpg', import.meta.url).href,
     imageAlt: 'Coastline with crashing waves'
   }]
