1ceb9d9f42350b4eb13040317417899ded3c3cfb
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-05-09 14:23:19 -0500

    add logos

--- overrides/home/partners.tsx
+++ overrides/home/partners.tsx
@@ -8,6 +8,8 @@ import { getLinkProps } from "$veda-ui-scripts/utils/url";
 
 //import haqastImg from "../media/haqast.png";
 import nasaImg from "../media/nasa.png";
+import epaImg from "../media/epa.svg";
+import noaaImg from "../media/noaa.png";
 
 
 
@@ -19,6 +21,7 @@ const LogoWrapper = styled.div`
   gap: 2rem;
   align-items: center;
   justify-content: center;
+  z-index: 10;
 
   > * {
     flex-shrink: 0;
@@ -35,11 +38,18 @@ export default function Partners(props: {
   const { variation, size } = props;
   const squareLogoHeight = size == "big"? "80" : "40"
   const wideLogoHeight = size == "big"? "28" : "14"
+  
 
   return (
     <LogoWrapper>
       <a {...getLinkProps("https://www.nasa.gov/")}>
-        <Image src={nasaImg} alt="NASA logo" height={squareLogoHeight} />
+        <Image src={nasaImg} alt="NASA logo" height={size === "big" ? "90" : "45"}  />
+      </a>
+      <a {...getLinkProps("https://www.epa.gov/")}>
+        <Image src={epaImg} alt="EPA logo" height={squareLogoHeight} />
+      </a>
+      <a {...getLinkProps("https://www.noaa.gov/")}>
+        <Image src={noaaImg} alt="NOAA logo" height={squareLogoHeight} />
       </a>
       {/* <a {...getLinkProps("https://haqast.org/")}>
         <Image src={haqastImg} alt="HAQAST logo" height={squareLogoHeight} />
