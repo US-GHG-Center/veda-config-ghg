3f60f8eb13f0360fdb835ee9d38cc044f2882020
Brian Freitag <freitagb@users.noreply.github.com>
2025-10-01 09:46:39 -0500

    Update veda.config.js

--- veda.config.js
+++ veda.config.js
@@ -171,7 +171,7 @@ module.exports = {
     contentId: 'gov-banner-content'
   },
   siteAlert: {
-    content: `Due to the lapse in federal government funding, NASA is not updating this website. We sincerely regret this inconvenience.`,
+    content: `Due to the lapse in federal government funding, the U.S. Greenhouse Gas Center is not updating this website. We sincerely regret this inconvenience.`,
     expires: '2026-08-03T12:00:00-04:00',
     type: 'emergency',
     slim: true,
